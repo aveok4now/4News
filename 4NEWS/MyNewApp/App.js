f606aae81dfdeaeabbf52fb33c101510f1fed2ef
2mpain <aveok4now@gmail.com>
2023-09-24 15:06:46 +0300

    Forgot Password Screen

--- 4NEWS/MyNewApp/App.js
+++ 4NEWS/MyNewApp/App.js
@@ -11,14 +11,14 @@ import { SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
-
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 
 const App = () => {
   return (
     <>
       <StatusBar backgroundColor="#B0ABD9" />
       <SafeAreaView style={styles.root}>
-        <ConfirmEmailScreen />
+        <ForgotPasswordScreen />
       </SafeAreaView>
     </>
   );
---
+++ 4NEWS/MyNewApp/src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton/CustomButton';
+
+// import { Container } from './styles';
+
+const ForgotPasswordScreen = () => {
+
+
+    const [username, setUsername] = useState('');
+
+
+    const onSendPressed = () => {
+        console.warn("Отправить");
+    }
+
+    const onSignInPress = () => {
+        console.warn("Вернуться к входу в аккаунт");
+    }
+
+    const onResendPress = () => {
+        console.warn("Повторный код");
+    }
+
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.root}>
+                <Text style={styles.title}>Сброс пароля</Text>
+                <CustomInput
+                    placeholder="Имя пользователя"
+                    value={username}
+                    setValue={setUsername}
+                />
+
+
+                <CustomButton
+                    text="Отправить"
+                    onPress={onSendPressed}
+                />
+
+
+                <CustomButton
+                    text="Вернуться к входу в аккаунт"
+                    onPress={onSignInPress}
+                    type="Tertiary"
+                />
+
+            </View>
+        </ScrollView>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: "#0400FC",
+        margin: 10,
+    },
+    text: {
+        fontSize: 12,
+        color: '#FCF7F8',
+        marginVertical: 10,
+        textAlign: 'center'
+    },
+    link: {
+        color: '#9FFFCB',
+    }
+
+})
+
+export default ForgotPasswordScreen;
