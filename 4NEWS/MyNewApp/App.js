cb5d4bdd76b12ddb7875fbbafe7a90d04f72397b
2mpain <aveok4now@gmail.com>
2023-09-24 15:13:02 +0300

    New Password Screen

--- 4NEWS/MyNewApp/App.js
+++ 4NEWS/MyNewApp/App.js
@@ -12,13 +12,14 @@ import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
+import NewPasswordScreen from './src/screens/NewPasswordScreen';
 
 const App = () => {
   return (
     <>
       <StatusBar backgroundColor="#B0ABD9" />
       <SafeAreaView style={styles.root}>
-        <ForgotPasswordScreen />
+        <NewPasswordScreen />
       </SafeAreaView>
     </>
   );
---
+++ 4NEWS/MyNewApp/src/screens/NewPasswordScreen/NewPasswordScreen.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton/CustomButton';
+
+// import { Container } from './styles';
+
+const NewPasswordScreen = () => {
+
+
+    const [code, setCode] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [newPasswordRepeat, setNewPasswordRepeat] = useState('');
+
+    const onSubmitPressed = () => {
+        console.warn("Подтвердить");
+    }
+
+    const onSignInPress = () => {
+        console.warn("Вернуться к входу в аккаунт");
+    }
+
+
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.root}>
+                <Text style={styles.title}>Сброс пароля</Text>
+
+                <CustomInput
+                    placeholder="Код подтверждения"
+                    value={code}
+                    setValue={setCode}
+                />
+                <CustomInput
+                    placeholder="Введите свой новый пароль"
+                    value={newPassword}
+                    setValue={setNewPassword}
+                    secureTextEntry
+                />
+
+                <CustomInput
+                    placeholder="Подтвердите новый пароль"
+                    value={newPasswordRepeat}
+                    setValue={setNewPasswordRepeat}
+                    secureTextEntry
+                />
+
+                <CustomButton
+                    text="Подтвердить"
+                    onPress={onSubmitPressed}
+                />
+
+
+                <CustomButton
+                    text="Вернуться к входу в аккаунт"
+                    onPress={onSignInPress}
+                    type="Tertiary"
+                />
+
+            </View>
+        </ScrollView>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: "#0400FC",
+        margin: 10,
+    },
+    text: {
+        fontSize: 12,
+        color: '#FCF7F8',
+        marginVertical: 10,
+        textAlign: 'center'
+    },
+    link: {
+        color: '#9FFFCB',
+    }
+
+})
+
+export default NewPasswordScreen;
