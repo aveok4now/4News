916ce60ed7da60de9e6339e4824af30daea751f4
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-10-19 13:58:21 +0300

    Revert "Tailwind configuration"
    This reverts commit c5a23b22ebc76e60f653d0345ab3d7e05b601c4c.

--- 4NEWS/MyNewApp/babel.config.js
+++ 4NEWS/MyNewApp/babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel"],
   // "react-native-sqlite-storage": {
   //   platforms: {
   //     android: {
