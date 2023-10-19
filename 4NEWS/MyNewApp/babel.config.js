c5a23b22ebc76e60f653d0345ab3d7e05b601c4c
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-10-19 12:46:08 +0300

    Tailwind configuration

--- 4NEWS/MyNewApp/babel.config.js
+++ 4NEWS/MyNewApp/babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ["nativewind/babel"],
   // "react-native-sqlite-storage": {
   //   platforms: {
   //     android: {
