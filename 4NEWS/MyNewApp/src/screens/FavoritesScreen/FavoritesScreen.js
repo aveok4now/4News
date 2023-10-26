ee0fe9ac039cefc9071c5fd75fbc5ebba95f58db
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-10-26 17:36:37 +0300

    Guests Favorites Block

--- 4NEWS/MyNewApp/src/screens/FavoritesScreen/FavoritesScreen.js
+++ 4NEWS/MyNewApp/src/screens/FavoritesScreen/FavoritesScreen.js
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Card from '../../components/Card';
+import * as Animatable from 'react-native-animatable'
+import { assets } from '../../../react-native.config';
+import TypeWriter from 'react-native-typewriter'
+import CustomButton from '../../components/CustomButton';
 
 
 export default function FavoritesScreen({ navigation }) {
     const [favorites, setFavorites] = useState([]);
+    const [identify, setIdenify] = useState('')
+    const [state] = useState("📰 Ваши сохранённые новости, ")
+
+
+    useEffect(() => {
+        const checkUserCredentials = async () => {
+            const savedUsername = await AsyncStorage.getItem('username');
+            const savedPassword = await AsyncStorage.getItem('password');
+            const guestID = await AsyncStorage.getItem('guestID');
+
+            if (savedUsername && savedPassword) {
+                //onSignInPressed({ username: savedUsername, password: savedPassword });
+                setIdenify(savedUsername)
+            } else if (savedUsername === 'guest') {
+                if (guestID) {
+                    setIdenify("Гость")
+                }
+            }
+        }
+
+        checkUserCredentials();
+    }, []);
+
 
     const loadFavorites = async () => {
         try {
@@ -21,16 +48,84 @@ export default function FavoritesScreen({ navigation }) {
         loadFavorites();
     }, []);
     return (
-        <View>
-            <Text>Favorites</Text>
-            <FlatList
-                data={favorites}
-                keyExtractor={(item) => item.url}
-                renderItem={({ item }) => {
-                    return <Card item={item} navigation={navigation} />;
-                }}
-            />
-        </View>
+        identify !== "Гость" ? (
+            <Animatable.View
+                animation="fadeIn"
+                duration={1000}
+            >
+
+                <View style={styles.header}>
+
+                    <TypeWriter
+                        style={styles.headerText}
+                        minDelay={10}
+                        typing={1}
+                    >
+                        {state}{identify}
+                    </TypeWriter>
+
+
+                    {/* <Text style={styles.headerText}>Ваши сохраннённые новости, {identify} </Text> */}
+                </View>
+
+                <FlatList
+                    data={favorites}
+                    keyExtractor={(item) => item.url}
+                    renderItem={({ item }) => {
+                        return <Card item={item} navigation={navigation} />;
+                    }}
+                />
+            </Animatable.View>
+        ) : (
+            <>
+                <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                    <Text style={styles.guestInfo}>Упс...</Text>
+                    <Text style={styles.guestSubInfo}>Чтобы просматривать сохранённые новости, необходимо войти в аккаунт</Text>
+
+                    <View style={{ width: '60%', marginVertical: 15 }}>
+                        <CustomButton
+                            text="Войти"
+                            onPress={() => navigation.navigate("Добро пожаловать !", { status: "logout" })}
+                        />
+                    </View>
+
+                </View>
+            </>
+        )
+
     );
 }
-
+const styles = StyleSheet.create({
+    heading: {
+        fontFamily: "Inter-Bold",
+        fontSize: 20,
+        paddingHorizontal: 20
+    },
+    header: {
+        // width: '100%',
+        // height: Dimensions.get("screen").height * 0.1
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#8EBBF3',
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15
+    },
+    headerText: {
+        fontFamily: "Inter-ExtraBold",
+        textAlign: 'justify',
+        fontSize: 20
+        //justifyContent: 'center'
+    },
+    guestInfo: {
+        textAlign: 'center',
+        fontFamily: 'Inter-ExtraBold',
+        fontSize: 24
+    },
+    guestSubInfo: {
+        textAlign: 'center',
+        fontFamily: 'Inter-Light',
+    }
+})
