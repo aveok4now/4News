509ce5dec9d4038ec27bc93b74e9a9cda59bf58b
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-11-01 22:25:54 +0300

    FeedBack Modal Upd

--- 4NEWS/MyNewApp/src/screens/FeedBackScreen/FeedBackScreen.js
+++ 4NEWS/MyNewApp/src/screens/FeedBackScreen/FeedBackScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, ScrollView, Linking, KeyboardAvoidingView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CustomDrawer from '../../components/customs/CustomDrawer'
 import LinearGradient from 'react-native-linear-gradient'
 import ModalPopup from '../../components/customs/CustomModal';
@@ -7,23 +7,48 @@ import CustomButton from '../../components/customs/CustomButton';
 import CustomInput from '../../components/customs/CustomInput';
 import { useForm } from 'react-hook-form';
 import * as Animatable from 'react-native-animatable'
-
+import TypeWriter from 'react-native-typewriter'
 
 export default function FeedBackScreen({ navigation }) {
     const [showModal, setShowModal] = useState(true)
     const [isMessageSend, setIsMessageSend] = useState(false)
 
+    const [isTyped, setIsTyped] = useState(false)
+
     const { control, handleSubmit, watch } = useForm();
     let message = watch('feedback');
 
+    const [countdown, setCountdown] = useState(4);
+
     const sendMessage = () => {
-        Linking.openURL(`mailto:sevsuphotoeditor@gmail.com?body=${encodeURIComponent(message)}`);
-        setShowModal(!showModal);
-        setIsMessageSend(!isMessageSend)
-        setTimeout(() => {
-            navigation.navigate("Домашняя страница")
-        }, 5000);
-    }
+        const subject = "Связь с 4News";
+        const mailtoUrl = `mailto:sevsufornews@gmail.com?subject=
+        ${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+        Linking.openURL(mailtoUrl);
+        setIsMessageSend(true);
+
+    };
+
+    const handleTypeComplete = () => setIsTyped(true)
+
+    useEffect(() => {
+
+        if (isTyped && countdown > 0) {
+            const timer = setInterval(() => {
+                setCountdown(countdown - 1);
+            }, 1000);
+
+            return () => clearInterval(timer);
+
+        }
+    }, [isTyped, countdown]);
+
+    useEffect(() => {
+        if (countdown === 0 && isTyped && navigation) {
+            navigation.navigate("Домашняя страница");
+        }
+    }, [countdown, isTyped, navigation]);
+
 
     return (
 
@@ -31,16 +56,16 @@ export default function FeedBackScreen({ navigation }) {
             colors={['rgba(58, 131, 244, 0.4)', 'rgba(9, 181, 211, 0.4)']}
             style={{ width: '100%', flex: 1, }}
         >
-            <CustomDrawer backgroundColor="#5b86e5" fgColor="#458fe6" elevation={35} navigation={navigation}>
-                <View style={styles.root}>
-                    {showModal ? (
-                        <>
-                            {/* {Alert.alert()} */}
-
-                            <ModalPopup
-                                navigation={navigation}
-                                visible={showModal}
-                            >
+            {/* <CustomDrawer backgroundColor="#5b86e5" fgColor="#458fe6" elevation={35} navigation={navigation}> */}
+            <View style={styles.root}>
+                {showModal && (
+                    <>
+                        {/* {Alert.alert()} */}
+                        <ModalPopup
+                            navigation={navigation}
+                            visible={showModal}
+                        >
+                            {!isMessageSend ? (
                                 <View>
                                     <Text style={{ textAlign: 'center', fontFamily: 'Inter-ExtraBold' }}>Оставьте свой отзыв и помогите нам стать лучше!
                                     </Text>
@@ -67,31 +92,45 @@ export default function FeedBackScreen({ navigation }) {
                                             text="Отправить"
                                             onPress={handleSubmit(sendMessage)}
                                         />
-                                        <CustomButton
-                                            type="Tertiary"
-                                            text="Назад"
-                                            onPress={() => {
-                                                setShowModal(!showModal)
-                                                navigation.navigate("Домашняя страница")
-                                            }}
-                                        />
+
 
                                         {/* <Text style={{ fontFamily: "Inter-ExtraBold" }}>ОК</Text> */}
                                     </View>
                                 </View>
-                            </ModalPopup>
+                            ) : (
+                                isMessageSend && (
+                                    <View style={{ justifyContent: 'center' }}>
+                                        <TypeWriter
+                                            style={styles.text}
+                                            minDelay={2}
+                                            typing={1}
+                                            onTypingEnd={handleTypeComplete}
+                                        >
+                                            Спасибо большое за Ваш отзыв!
+                                        </TypeWriter>
+                                    </View>
+                                )
+                            )
+                            }
 
-                        </>
-                    ) : (
-                        isMessageSend && (
-                            <View>
-                                <Text style={{ color: 'green' }}>Спасибо за отзыв!</Text>
-                            </View>
-                        )
-                    )
-                    }
-                </View>
-            </CustomDrawer>
+                            <Animatable.View animation="fadeIn" style={{ justifyContent: 'center' }}>
+                                <CustomButton
+                                    type="Tertiary"
+                                    text="Назад"
+                                    onPress={() => {
+                                        setShowModal(!showModal)
+                                        navigation.navigate("Домашняя страница")
+                                    }}
+                                />
+                                {isTyped && <Text style={{ fontFamily: 'Inter-ExtraLight', opacity: 0.5, textAlign: 'center' }}>{countdown}</Text>}
+                            </Animatable.View>
+                        </ModalPopup>
+
+                    </>
+                )
+                }
+            </View>
+            {/* </CustomDrawer> */}
         </LinearGradient>
     )
 }
@@ -101,5 +140,13 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+
+    },
+    text: {
+        fontFamily: 'Inter-ExtraBold', color: 'white', textShadowColor: 'rgba(0, 0, 0, 0.25)',
+        textShadowOffset: { width: 0, height: 2 },
+        textShadowRadius: 4,
+        textAlign: 'center',
+        fontSize: 16
     }
-})
+});
