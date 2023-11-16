df7a64c555f6faeb60d80bb982246c165a815a84
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-11-16 20:09:51 +0300

    CustomCarousel upd

--- src/components/customs/CustomCarousel/CustomCarousel.js
+++ src/components/customs/CustomCarousel/CustomCarousel.js
@@ -7,49 +7,42 @@ import {
     StatusBar,
     FlatList,
     Animated,
-    TouchableOpacity
+    TouchableOpacity,
 } from 'react-native';
 import React, { Component, useRef } from 'react';
-import { Images } from '../../../constants/Images';
-import { Captions } from '../../../constants/Images';
+import { Images, Captions, ruCaptions } from '../../../constants/Images';
+
 
 const { width, height } = Dimensions.get('window');
 const ITEM_SIZE = width * 0.5;
 const SPACING = 5;
 
-
-
 export default function CustomCarousel({ navigation, apiKeyList, apiKeyIndex }) {
     const scrollX = useRef(new Animated.Value(0)).current;
     const NewsImages = [{ key: 'left-spacer' }, ...Images, { key: 'right-spacer' }];
 
-
-
-
-    const handleCategoryPressed = (curCaption) => {
+    const handleCategoryPressed = curCaption => {
         switch (curCaption) {
-            case "Weather":
-                navigation.navigate("Weather Screen")
+            case 'Weather':
+                navigation.navigate('Weather Screen');
                 break;
             default:
                 newsByCategoryNavigation(curCaption);
                 break;
         }
+    };
 
-
-    }
-
-    const newsByCategoryNavigation = (curCaption) => {
+    const newsByCategoryNavigation = curCaption => {
         try {
             navigation.navigate('Новости по категориям', {
                 apiKeyList: apiKeyList,
                 apiKeyIndex: apiKeyIndex,
                 caption: curCaption,
-            })
+            });
         } catch (err) {
-            console.warn(err)
+            console.warn(err);
         }
-    }
+    };
 
     return (
         <View style={styles.container}>
@@ -97,11 +90,13 @@ export default function CustomCarousel({ navigation, apiKeyList, apiKeyIndex }) 
                                     opacity,
                                 },
                             ]}>
-                            <TouchableOpacity activeOpacity={0.75} onPress={() => handleCategoryPressed(Captions[index - 1])}>
+                            <TouchableOpacity
+                                activeOpacity={0.75}
+                                onPress={() => handleCategoryPressed(Captions[index - 1])}>
                                 <View style={styles.newsInner}>
                                     <Image source={{ uri: item }} style={styles.newsImage} />
 
-                                    <Text style={styles.caption}>{Captions[index - 1]}</Text>
+                                    <Text style={styles.caption}>{ruCaptions[index - 1]}</Text>
                                 </View>
                             </TouchableOpacity>
                         </Animated.View>
@@ -132,18 +127,18 @@ const styles = StyleSheet.create({
         borderRadius: 24,
         margin: 0,
         marginBottom: 10,
-        marginTop: '15%'
+        marginTop: '15%',
     },
     spacerContainer: {
         height: 200,
         width: width - ITEM_SIZE / 2,
     },
     caption: {
-        color: "white",
-        fontFamily: "Inter-Light",
+        color: 'white',
+        fontFamily: 'Inter-Light',
         fontSize: 20,
         textShadowColor: 'rgba(226, 232, 240, 0.25)',
         textShadowOffset: { width: 0, height: 5 },
         textShadowRadius: 4,
-    }
+    },
 });
--- src/constants/Images.js
+++ src/constants/Images.js
@@ -5,15 +5,38 @@ export const Images = [
     'https://i.pinimg.com/564x/90/e2/ae/90e2ae374fbeeedc045c2ce0535aaaca.jpg',
     'https://i.pinimg.com/564x/ac/dc/05/acdc05cc1bff5f523556c6c5ca2e4a0a.jpg',
     'https://i.pinimg.com/564x/b7/c9/d8/b7c9d86423dcec48e5daccd0eee7b278.jpg',
+    'https://i.pinimg.com/564x/02/55/ab/0255ab8d033408ebe61fa808084e4d36.jpg',
+    'https://i.pinimg.com/564x/28/f6/1d/28f61d398c8a28f38ee77f012ffdcfce.jpg',
     'https://i.pinimg.com/564x/00/c2/5d/00c25d12c2f39097afc3548de57d316e.jpg',
+    'https://i.pinimg.com/564x/14/f7/5d/14f75d3a80391ae5d944ff59508a3285.jpg',
+    'https://i.pinimg.com/736x/dc/97/4e/dc974e453e02886762d75825dca5c7f3.jpg'
 ];
 
 export const Captions = [
-    'Bitcoin',
+    'bitcoin',
     'IT',
-    'Games',
-    'Movies',
-    'Weather',
+    'games',
+    'movies',
+    'weather',
     'Apple',
-    'Music',
+    'microsoft',
+    'sony',
+    'music',
+    'cars',
+    'travelling'
+
+];
+
+export const ruCaptions = [
+    'Биткоин',
+    'IT',
+    'Игры',
+    'Кино',
+    'Погода',
+    'Apple',
+    'Microsoft',
+    'Sony',
+    'Музыка',
+    'Автомобили',
+    'Путешествия'
 ];
