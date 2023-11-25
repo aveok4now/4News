346952a3564b32df106b896d4ec8628e3b9b0853
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-11-25 20:39:20 +0300

    feat: Added new news categories

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -7,13 +7,20 @@ export const Images = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp9l1aqt2R0-_I6qe38vTtn8CKFPstphaC60TwBIoPra4IQckG-C8VsrkLWbbTWmzQ-jk&usqp=CAU', // Газета
     'https://podpiska.pochta.ru/storage/public/16dfd494-1592-4fe6-ac16-8584cae1a0bb/file_2207e7df-eff5-4d80-90e9-5b497cd93f36_EXTRA_LARGE.jpg', // Ведомости
     'https://play-lh.googleusercontent.com/FrrgbblwQmbQvkt4RDsX_SieFc0ybagCOLfdDH3bVtv-DnzLgiuj5JtQjv7Iow1lk84', // Пикабу
+    'https://yt3.googleusercontent.com/ytc/APkrFKaqXbovvHRJ7rWbC4t01wQu7wl2BXB-Tt-2s4k=s176-c-k-c0x00ffffff-no-rj', // Ferra.ru
     'https://static.rustore.ru/apk/2063488014/content/ICON/90acd8d4-2a86-4b81-b1ad-5378dd1a6501.png', // Коммерсантъ
     'https://itforum.admhmao.ru/upload/iblock/c65/gu0u5cjxtmyqptzbwo4nq8rwl3yx7am3/tass.jpg', // ТАСС
     'https://sun1-57.userapi.com/impg/ocMA0CbYzepChHqvJwGHUKLGp0PHSuEde46lsg/yd0P7X8_PLo.jpg?size=1204x1204&quality=95&sign=eff094c05a0a628b55ead94b007f59a0&c_uniq_tag=Hc49Ag7dcTvV0fK3fTb00BtlYvd0k8eU7zYg0YFGciw&type=album', // Известия 
     'https://store-images.s-microsoft.com/image/apps.25801.9007199266633218.dce2f6c0-133e-4626-8451-f0dbce8acee2.514c7db1-fd6b-4e2d-b904-c694b148532d', // IXBT
+    'https://play-lh.googleusercontent.com/XajZeVHWvAVwrtyx-lvvzB23oPtD9YPY8S_cwPGGhgOMcMK5oZ20nXbAJKGbyXd9Ln8', // РГ РУ
+    'https://lh3.googleusercontent.com/uqnLSKgWf4JIiQSZXED9Wf0KERKvMTgskp8-74oXwGzWuyWFNWu7B78qTT6LnQDGwLQ', // Медуза
+    'https://freesoft.ru/storage/images/763/7628/762791/762791_normal.png', // Интерфакс
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXMnmqXVMGqNkBaAgjy0E9zI7UmC0njSsfHcbNYdEflcGaQOgdB2SptyzFssOShNcmFmg', // LIFE RU
+    'https://avatars.dzeninfra.ru/get-zen-logos/201842/pub_5a324ec3830905958b6dbdee_5a3256734bf161de6f650be7/xxh', // ВЗГЛЯД
     'https://yt3.googleusercontent.com/kOEZnW38w4tXcTTwvuC3xi-8RAmvxh5lNVub56V_14x5o773zkVGS450nC5yfYUYLvyxK9zEXA=s900-c-k-c0x00ffffff-no-rj', // Billboard
     'https://yt3.googleusercontent.com/b2Rf6ON5kiJEiq8EvYWYuQcX8M_c2nODMPcJMKW4-iY8iOY2AHDHS6JT47bKSY9AlVg3tOOWJPE=s900-c-k-c0x00ffffff-no-rj', // КиноПоиск
     'https://play-lh.googleusercontent.com/80yGc-oEinV3tSsBRV2VyKEheiSljzdghw6v8NE-fXQAtpLzJgHtVeAA2ZtiA7P93jv1', // Onliner
+    'https://avatars.dzeninfra.ru/get-zen-logos/223306/pub_5a6ed86edcaf8e581b8d4324_5e43c5aadf9304340b26205f/xxh', // F1NEWS.RU
     'https://static01.nyt.com/images/2021/06/14/business/13bizsky-ontech-print/08OnTech-YouTube-mediumSquareAt3X-v2.jpg', // YouTube
 
     'https://i.pinimg.com/736x/af/92/5c/af925c4a5ded9b5b89d24410a21c58f8.jpg', // Валюта
@@ -40,13 +47,20 @@ export const Captions = [
     'Gazeta.ru',
     'Vedomosti.ru',
     'Pikabu.ru',
+    'Ferra.ru',
     'Kommersant.ru',
     'Tass.ru',
     'IZ.ru',
     'Ixbt.com',
+    'RGRU',
+    'Meduza',
+    'Interfax.ru',
+    'LIFE',
+    'VZ.RU',
     'Billboard',
     'Kinopoisk.ru',
     'Onliner.by',
+    'F1NEWS.RU',
     'Youtube.com',
 
     'currency',
@@ -73,13 +87,20 @@ export const ruCaptions = [
     'Газета.Ru',
     'Ведомости',
     'Пикабу',
+    'Ferra.ru',
     'Коммерсантъ',
     'ТАСС',
     'Известия',
     'IXBT',
+    'RGRU',
+    'Медуза',
+    'Интерфакс',
+    'LIFE',
+    'ВЗГЛЯД',
     'Биллборд',
     'КиноПоиск',
     'Onliner',
+    'F1NEWS.RU',
     'YouTube',
 
     'Валюта',
--- src/screens/NewsOverviewScreen/NewsOverviewScreen.js
+++ src/screens/NewsOverviewScreen/NewsOverviewScreen.js
@@ -44,12 +44,19 @@ const NewsOverviewScreen = ({ route, navigation }) => {
         'YouTube': 'youtube.com',
         'КиноПоиск': 'kinopoisk.ru',
         'Пикабу': 'pikabu.ru',
+        'Ferra.ru': 'ferra.ru',
         'Игромания': 'igromania.ru',
         'Ведомости': 'vedomosti.ru',
         'Биллборд': 'billboard.com',
         'Коммерсантъ': 'kommersant.ru',
         'Канобу': 'kanobu.ru',
-        'Apple': 'apple.com'
+        'Apple': 'apple.com',
+        'F1NEWS.RU': 'f1news.ru',
+        'RGRU': 'rg.ru',
+        'Медуза': 'meduza.io',
+        'Интерфакс': 'interfax.ru',
+        'LIFE': 'life.ru',
+        'ВЗГЛЯД': 'vz.ru',
     };
 
 
