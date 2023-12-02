a93d825c376662760d3665b0526006aaf47df08a
2mpain <123462736+dtb4life@users.noreply.github.com>
2023-12-02 21:35:56 +0300

    feat: Implemented NewsFooter component
    Footer component with stack carousel of  pressable images with animated titles

--- src/components/UsersNewsComponents/GroupsList/GroupsList.js
+++ src/components/UsersNewsComponents/GroupsList/GroupsList.js
@@ -1,9 +1,10 @@
 import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import React from 'react';
 import { width, height } from '../../../utils/getDimensions';
-import { image500 } from '../../../api/moviedb';
+import * as Animatable from 'react-native-animatable';
+import AnimatedText from '../AnimatedText';
 
-export default function GroupsList({ item, navigation }) {
+export default function GroupsList({ item, navigation, activeSlide, title }) {
     const handleCardPress = () => {
         navigation.navigate('NewsViewer', {
             url: item.link,
@@ -11,15 +12,21 @@ export default function GroupsList({ item, navigation }) {
     };
 
     return (
-        <TouchableWithoutFeedback onPress={handleCardPress}>
-            <Image
-                source={{ uri: item.image }}
-                style={{
-                    width: width * 0.6,
-                    height: height * 0.3,
-                    borderRadius: 24,
-                }}
-            />
-        </TouchableWithoutFeedback>
+        <View>
+            <TouchableWithoutFeedback onPress={handleCardPress}>
+                <Image
+                    source={{ uri: item.image }}
+                    style={{
+                        width: width * 0.6,
+                        height: height * 0.3,
+                        borderRadius: 24,
+                    }}
+                />
+            </TouchableWithoutFeedback>
+
+            {activeSlide === item.id - 1 && (
+                <AnimatedText activeSlide={activeSlide} title={title} />
+            )}
+        </View>
     );
 }
--- src/components/UsersNewsComponents/NewsFooter/NewsFooter.js
+++ src/components/UsersNewsComponents/NewsFooter/NewsFooter.js
@@ -1,16 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Carousel from 'react-native-snap-carousel'
-import GroupsList from '../GroupsList'
-import { groupsData, setGroupsData } from '../../../screens/UsersNewsScreen/groupsData'
-import { width } from '../../../utils/getDimensions'
+import React, { useState, useRef } from 'react';
+import { View, Text } from 'react-native';
+import { Carousel, Pagination } from 'react-native-snap-carousel';
+import GroupsList from '../GroupsList';
+import {
+    groupsData,
+    setGroupsData,
+} from '../../../screens/UsersNewsScreen/groupsData';
+import { width } from '../../../utils/getDimensions';
 
-export default function NewsFooter({ item, navigation }) {
+export default function NewsFooter({ navigation }) {
+
+    const [activeSlide, setActiveSlide] = useState(0);
+
+
+    const pagination = () => {
+        return (
+            <Pagination
+                dotsLength={groupsData.length}
+                activeDotIndex={activeSlide}
+                containerStyle={{ alignSelf: 'center' }}
+                dotStyle={{
+                    width: 10,
+                    height: 10,
+                    borderRadius: 5,
+                    marginHorizontal: 8,
+                    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+                }}
+                inactiveDotStyle={
+                    {
+                        backgroundColor: 'blue'
+                    }
+                }
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+            />
+        );
+    };
+
     return (
         <View
             style={{
-                width: width,
+                flex: 1,
+                width: '100%',
                 paddingBottom: 8,
+                justifyContent: 'center',
             }}>
             <Text
                 style={{
@@ -21,22 +54,29 @@ export default function NewsFooter({ item, navigation }) {
                     textShadowColor: 'rgba(226, 232, 240, 0.25)',
                     textShadowOffset: { width: 0, height: 3 },
                     textShadowRadius: 4,
+                    marginBottom: 8,
                 }}>
                 Вам также может понравится:
             </Text>
             <Carousel
+                autoplay={true}
+                autoplayDelay={2000}
+                autoplayInterval={3000}
                 data={groupsData}
-                renderItem={({ item }) => (
-                    <GroupsList item={item} navigation={navigation} />
+                renderItem={({ item, index }) => (
+                    <GroupsList item={item} navigation={navigation} activeSlide={activeSlide} title={item.title} />
                 )}
-                firstItem={1}
+                firstItem={0}
                 inactiveSlideOpacity={0.5}
                 sliderWidth={width}
+                sliderHeight={width}
                 itemWidth={width * 0.62}
                 slideStyle={{ display: 'flex', alignItems: 'center' }}
                 layout={'stack'}
-                layoutCardOffset={`18`}
+                layoutCardOffset={`24`}
+                onSnapToItem={index => setActiveSlide(index)}
             />
+            {pagination()}
         </View>
-    )
+    );
 }
